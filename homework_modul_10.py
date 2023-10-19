b443104b282c00b047f1438676c091e6992a0c6e
OlenaSakhanda1 <130360301+OlenaSakhanda1@users.noreply.github.com>
2023-10-19 22:44:13 +0300

    Add files via upload

---
+++ homework_modul_10.py
@@ -0,0 +1,319 @@
+import os, re, csv, string, json
+from homework_modul_4_2 import normalizing_letter_cases_in_text
+from collections import Counter
+from datetime import datetime, timedelta
+import xml.etree.ElementTree as ET
+import sqlite3
+
+class Record:
+    def __init__(self, text):
+        self.text = text
+        self.date_published = datetime.now()
+
+class News(Record):
+    def __init__(self, text, city):
+        super().__init__(text)
+        self.city = city
+
+    def publish(self):
+        return f"{self.__class__.__name__}-------------------------\n {self.text}\n {self.city},  {self.date_published}\n"
+
+class Private_ad(Record):
+    def __init__(self, text, expiration_date):
+        super().__init__(text)
+        self.expiration_date = datetime.strptime(expiration_date, '%Y-%m-%d').date()
+        self.days_left = (self.expiration_date - datetime.now().date()).days
+
+    def publish(self):
+        return f"{self.__class__.__name__}-------------------------\n {self.text}\n Actual until: {self.expiration_date}, {self.days_left} days left\n"
+
+class Tasks(Record):
+    def __init__(self, text, group, priority, story_points):
+        super().__init__(text)
+        self.group = group
+        self.priority = priority
+        self.story_points = story_points
+
+    def publish(self):
+        return f"{self.__class__.__name__}-------------------------\n {self.text}\n group: {self.group}\n priority: {self.priority}\n story_points: {self.story_points}\n"
+
+class Save_to_bd(Record):
+    def __init__(self, text):
+        super().__init__(text)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS News (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT,
+                       city TEXT, date_published TEXT, UNIQUE (text, city)); """)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Private_ad (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT,
+                       expiration_date TEXT, date_published TEXT, UNIQUE (text, expiration_date)); """)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, [group] TEXT,
+                       priority INTEGER, story_points INTEGER, UNIQUE (text, [group], priority, story_points)); """)
+        conn.commit()
+
+    def publish_news(self, city):
+        cursor.execute("INSERT INTO News (text, city, date_published) VALUES (?, ?, ?)", (self.text, city, self.date_published))
+        conn.commit()
+
+    def publish_ad(self, expiration_date):
+        cursor.execute("INSERT INTO Private_ad (text, expiration_date, days_left) VALUES (?, ?, ?)", (self.text, expiration_date, (expiration_date - self.date_published.date()).days))
+        conn.commit()
+
+    def publish_task(self, group, priority, story_points):
+        cursor.execute("INSERT INTO Tasks (text, group, priority, story_points) VALUES (?, ?, ?, ?)", (self.text, group, priority, story_points))
+        conn.commit()
+
+class Records_from_file:
+    def __init__(self, file_path, num_records):
+        self.file_path = file_path
+        self.num_records = num_records
+
+    def read_write_records(self):
+        records = []
+        record_section = []
+        try:
+            with open(self.file_path, "r") as file:
+                for line in file:
+                    line = line.strip()
+                    if line:
+                        record_section.append(normalizing_letter_cases_in_text(line))
+                    else:
+                        if record_section:
+                            records.append(record_section)
+                        record_section = []
+
+                if record_section:
+                    records.append(record_section)
+
+            #os.remove(self.file_path)
+        except FileNotFoundError:
+            print(f"File '{self.file_path}' not found.")
+        except Exception as e:
+            print(f"An error occurred while reading the file: {str(e)}")
+
+        if self.num_records >= len(records):
+            self.num_records = len(records)
+        records = records[:self.num_records]
+        for i in range(len(records)):
+            if records[i][0] == 'News-------------------------':
+                text = records[i][1]
+                city = records[i][2][0:records[i][2].find(',')]
+                news = News(text, city)
+                news.publish()
+                save_record_to_file(news)
+            elif records[i][0] == 'Private_ad-------------------------':
+                text = records[i][1]
+                expiration_date_str = datetime.strptime(records[i][2][14:24], '%Y-%m-%d').date()
+                days_to_subtract = int(records[i][2][26:records[i][2].rfind("'") - 9])
+                new_expiration_datetime = datetime.combine(expiration_date_str, datetime.min.time()) - timedelta(days=days_to_subtract)
+                expiration_date = str(new_expiration_datetime.date())
+                private_ad = Private_ad(text, expiration_date)
+                private_ad.publish()
+                save_record_to_file(private_ad)
+            elif records[i][0] == 'Tasks-------------------------':
+                text = records[i][1]
+                group = records[i][2][7::]
+                priority = records[i][3][10::]
+                story_points = records[i][4][14::]
+                task = Tasks(text, group, priority, story_points)
+                task.publish()
+                save_record_to_file(task)
+        return records
+
+class Records_from_file_json:
+    def __init__(self, file_path, num_records):
+        self.file_path = file_path
+        self.num_records = num_records
+
+    def read_records(self):
+        try:
+            with open(self.file_path, "r") as f:
+                my_dict = json.load(f)
+
+        except FileNotFoundError:
+            print(f"File '{self.file_path}' not found.")
+            return []
+        return my_dict
+
+    def write_records_to_json(self, my_dict, file_path):
+        try:
+            with open(output_file, "a") as file:
+                for i, (key, value) in enumerate(my_dict.items()):
+                    if i >= self.num_records or self.num_records >= len(my_dict):
+                        break
+                    if key.startswith('News'):
+                        text = value['text']
+                        city = value['city']
+                        news = News(text, city)
+                        news.publish()
+                        file.write(news.publish() + "\n")
+                    elif key.startswith('Private_ad'):
+                        text = value['text']
+                        expiration_date = value['expiration_date']
+                        private_ad = Private_ad(text, expiration_date)
+                        private_ad.publish()
+                        file.write(private_ad.publish() + "\n")
+                    elif key.startswith('Tasks'):
+                        text = value['text']
+                        group = value['group']
+                        priority = value['priority']
+                        story_points = value['story_points']
+                        task = Tasks(text, group, priority, story_points)
+                        task.publish()
+                        file.write(task.publish() + "\n")
+            print(f"Records written to {output_file} successfully!")
+            # os.remove(self.file_path)
+        except Exception as e:
+            print(f"An error occurred while writing to the file: {str(e)}")
+
+class Records_from_file_xml(Records_from_file_json):
+    def __init__(self, file_path, num_records):
+        self.file_path = file_path
+        self.num_records = num_records
+
+    def read_records(self):
+        tree = ET.parse('output.xml')
+        root = tree.getroot()
+        my_dict = {}
+
+        # Loop through the XML tree and populate the dictionary
+        for child in root:
+            sub_dict = {}
+            for sub_child in child:
+                sub_dict[sub_child.tag] = sub_child.text
+            my_dict[child.tag] = sub_dict
+        self.write_records_to_json(my_dict, file_path)
+
+
+def save_record_to_file(record):
+    with open('News_feeds1.txt', "a") as file:
+        file.write(record.publish() + "\n")
+    count_records('News_feeds1.txt')
+
+def count_records(file_path):
+    records = []
+    record_section = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                record_section.append(line)
+            else:
+                if record_section:
+                    records.append(record_section)
+                record_section = []
+
+        if record_section:
+            records.append(record_section)
+
+    dict_words = []
+    dict_words1 = []
+    for i in range(len(records)):
+        for j in range(len(records[i])):
+            temp_word = records[i][j].split()
+            for k in range(len(temp_word)):
+                translator = str.maketrans("", "", string.punctuation)
+                result = temp_word[k].translate(translator)
+                dict_words.append(result)
+                dict_words1.append(result.lower())
+
+    word_counts = Counter(dict_words1)
+    word_counts_dict = dict(word_counts)
+    total_letters = sum(len(word) for word in dict_words)
+    uppercase_letters = sum(1 for letter in ''.join(dict_words) if letter.isupper())
+    percentage_uppercase = round((uppercase_letters / total_letters) * 100, 2) if total_letters > 0 else 0
+
+    letter_counts = {}
+    for word in dict_words:
+        for letter in word:
+            if letter.isalpha():
+                letter_counts[letter] = letter_counts.get(letter, 0) + 1
+
+    csv_filename = 'word_counts.csv'
+    with open(csv_filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Word', 'Count'])
+        for word, count in word_counts_dict.items():
+            csv_writer.writerow([word, count])
+        csv_writer.writerow([])
+        csv_writer.writerow(['Letter', 'Count'])
+        for letter, count in letter_counts.items():
+            csv_writer.writerow([letter, count])
+        csv_writer.writerow([])
+        csv_writer.writerow(['All letters (upper and lower cases): ' + str(total_letters)])
+        csv_writer.writerow(['All letters in upper case): ' + str(uppercase_letters)])
+        csv_writer.writerow(['Percentage letters in upper case to all letters: ' + str(percentage_uppercase)])
+
+output_file = 'News_feeds1.txt'
+file_path = input("Enter the txt file's path for rewriting records: ")
+if len(file_path) < 1:
+    file_path = 'News_feeds.txt'
+num_records = int(input("Choose how many records from txt file do you want to write in the constant file: "))
+r = Records_from_file(file_path, num_records)
+r.read_write_records()
+
+file_path_json = input("Enter the file's path for rewriting records from json: ")
+if len(file_path_json) < 1:
+    file_path_json = 'News_feeds.json'
+num_records_json = int(input("Choose how many records from json file do you want to write in the constant file: "))
+r = Records_from_file_json(file_path_json, num_records_json)
+records = r.read_records()
+r.write_records_to_json(records, output_file)
+
+file_path_xml = input("Enter the file's path for rewriting records from xml: ")
+if len(file_path_xml) < 1:
+    file_path_xml = 'output.xml'
+num_records_xml = int(input("Choose how many records from xml file do you want to write in the constant file: "))
+r = Records_from_file_xml(file_path_xml, num_records_xml)
+records = r.read_records()
+r.write_records_to_json(records, output_file)
+
+while True:
+    conn = sqlite3.connect("dqe1.db")
+    cursor = conn.cursor()
+    print("Choose the record type:")
+    print("1. News")
+    print("2. Private Ad")
+    print("3. Tasks")
+    print("4. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        text = input("Enter news text: ")
+        city = input("Enter city: ")
+        news = News(text, city)
+        news.publish()
+        save_record_to_file(news)
+        record = Save_to_bd(text)
+        record.publish_news(city)
+        print("News published successfully!")
+    elif choice == "2":
+        text = input("Enter ad text: ")
+        expiration_date = input("Enter expiration date (YYYY-MM-DD): ")
+        private_ad = Private_ad(text, expiration_date)
+        private_ad.publish()
+        save_record_to_file(private_ad)
+        record = Save_to_bd(text)
+        record.publish_ad(expiration_date)
+        print("Private Ad published successfully!")
+    elif choice == "3":
+        text = input("Enter task text: ")
+        group = input("Enter group for the task: ")
+        priority = input("Enter priority for the task: ")
+        story_points = input("Enter story_points for the task: ")
+        task = Tasks(text, group, priority, story_points)
+        task.publish()
+        save_record_to_file(task)
+        record = Save_to_bd(text)
+        record.publish_task(group, priority, story_points)
+        print("Task published successfully!")
+    elif choice == "4":
+        count_records('News_feeds1.txt')
+        conn.close()
+        break
+    else:
+        print("Invalid choice. Please choose a valid option.")
+    count_records('News_feeds1.txt')
+    conn.close()
+
+
+
+
